bbde83f12503d87d4613fc6d07661d9f72df37e1
tvblackman1 <vbif.misha2012@yandex.ru>
2025-06-03 19:55:18 +0300

    fixes

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -8,10 +8,8 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPool2D,Flatten, Dropout
 
-# Callbacks
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-# Loading the Dataset 
 (X_train,y_train), (X_test,y_test) = mnist.load_data()
 
 X_train.shape, y_train.shape, X_test.shape, y_test.shape
@@ -22,25 +20,14 @@ def plot_input_image(i):
     plt.axis('off')
     plt.show()
 
-# Pre-processing
-
-# Normalizing the image to [0,1] range
-
 X_train = X_train.astype(np.float32)/255
 X_test = X_test.astype(np.float32)/255
-
-# Reshaping or Expanding the dimensions of image from
-# (28,28) to (28,28,1)
 
 X_train = np.expand_dims(X_train,-1)
 X_test = np.expand_dims(X_test,-1)
 
-# One-hot encoding
-
 y_train = keras.utils.to_categorical(y_train)
 y_test = keras.utils.to_categorical(y_test)
-
-# Building the Model
 
 model = Sequential()
 model.add(Conv2D(32,(3,3),input_shape=(28,28,1),activation='relu'))
@@ -53,7 +40,6 @@ model.add(Conv2D(64,(3,3),activation='relu'))
 model.add(Flatten())
 
 model.add(Dropout(0.25))
-# 25 % value is dropped ; can also try with value 0.5 for 50 % to be dropped
 
 model.add(Dense(64,activation='relu'))
 model.add(Dense(10,activation='softmax'))
@@ -62,15 +48,12 @@ model.summary()
 
 model.compile(optimizer='adam',loss=keras.losses.categorical_crossentropy,metrics=['accuracy'])
 
-# EarlyStopping
 es = EarlyStopping(monitor='val_accuracy', min_delta=0.01, patience=4, verbose=1)
 
-# Model Checkpoint
 mc = ModelCheckpoint("./model.h5", monitor='val_accuracy', verbose=1, save_best_only=True)
 
 cb = [es,mc]
 
-# Model Training 
 model.fit(X_train, y_train, epochs=10, validation_split=0.3, callbacks=cb)
 
 loss,accuracy = model.evaluate(X_test,y_test)
--- test.py
+++ test.py
@@ -1,26 +1,16 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
 from keras.models import load_model
 
 MODEL = load_model("model.h5")
 image_no = 9
 
 for image_no in range(1, 10):
-    
-    # input_img = cv2.imread('sample_images/image_{}.png'.format(image_no))
     input_img = cv2.imread('test_images/{}.png'.format(image_no))
 
-    # print(input_img)
-    # print(input_img.shape)
-
     gray = cv2.cvtColor(input_img,cv2.COLOR_BGR2GRAY)
-    # print(gray.shape)
 
     image_resize = cv2.resize(gray,(28,28))
-    # print(image_resize.shape)
-
     image_resize = image_resize/255
     image_reshape = np.reshape(image_resize,[1,28,28])
 
